746de2675ef7ed43f7b790a2d8efa6befea85fb5
qhuengi <justinian55@gmail.com>
2020-06-23 17:52:55 -0400

    added comments

--- BRthreshold.py
+++ BRthreshold.py
@@ -1,13 +1,18 @@
 import cv2 as cv
 import numpy as np
 
-cloud=cv.imread(r"D:\Users\me\Downloads\cloud.jpg")
+filename=r"D:\Users\me\Downloads\cloud.jpg"
+cloud=cv.imread(filename)
+
+# returns normalized blue/red ratio
 def ratio(pixel):
+    # sets red pixel intensity to 1 to avoid division by 0
     if pixel[2]==0:
 	    pixel[2]=1
     brRatio=pixel[0]/pixel[2]
     return (brRatio-1)/(brRatio+1)
 
+# applies function to each pixel of image, kind of stupid
 def imageMap(image,func):
     mapped=np.empty(image.shape[0:2])
     for i in range(0,image.shape[0]):
@@ -17,9 +22,7 @@ def imageMap(image,func):
 
 thresholdValue=0.25
 
-#ratioDebug=lambda x:int(255*ratio(x))
 cloudRatio=imageMap(cloud,ratio)
-#cloudRatioDebug=imageMap(cloud,ratioDebug).astype(np.int8)
 ret,thresh=cv.threshold(cloudRatio,.25,1,cv.THRESH_BINARY)
 cv.imshow('threshold',thresh)
 cv.imshow('orig',cloud)
