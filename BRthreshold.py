5292c87f836659425fd0c7bfd7958f22a0943a55
qhuengi <justinian55@gmail.com>
2020-06-24 12:09:53 -0400

    refactored BR threshold as module.

--- BRthreshold.py
+++ BRthreshold.py
@@ -1,28 +1,21 @@
 import cv2 as cv
 import numpy as np
 
-filename=r"D:\Users\me\Downloads\cloud.jpg"
-cloud=cv.imread(filename)
 
-# returns normalized blue/red ratio
-def ratio(pixel):
-    # sets red pixel intensity to 1 to avoid division by 0
-    if pixel[2]==0:
-	    pixel[2]=1
-    brRatio=pixel[0]/pixel[2]
-    return (brRatio-1)/(brRatio+1)
+def brThreshold(image, threshold=0.25):
+    """Thresholds image by normalized blue/red ratio (see Li et al., 2011).
 
-# applies function to each pixel of image, kind of stupid
-def imageMap(image,func):
+    Keyword arguments:
+    threshold -- threshold value for ratio, between 0 and 1, default 0.25.
+    """
     mapped=np.empty(image.shape[0:2])
     for i in range(0,image.shape[0]):
         for j in range(0,image.shape[1]):
-            mapped[i][j]=func(image[i][j])
-    return(mapped)
-
-thresholdValue=0.25
-
-cloudRatio=imageMap(cloud,ratio)
-ret,thresh=cv.threshold(cloudRatio,.25,1,cv.THRESH_BINARY)
-cv.imshow('threshold',thresh)
-cv.imshow('orig',cloud)
+            pixel=image[i][j]
+            # sets red pixel intensity to 1 to avoid division by 0
+            if pixel[2]==0:
+                pixel[2]=1
+            brRatio=pixel[0]/pixel[2]
+            mapped[i][j]=(brRatio-1)/(brRatio+1)
+    ret, thresh = cv.threshold(mapped, threshold, 1, cv.THRESH_BINARY)
+    return thresh
