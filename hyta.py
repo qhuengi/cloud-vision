0fdd3325a92f1885af0f3ac92c7e02e1cb790a64
qhuengi <justinian55@gmail.com>
2020-06-30 12:04:24 -0400

    Debugged hyta.py.

--- hyta.py
+++ hyta.py
@@ -1,5 +1,6 @@
 import brmap
 import cv2 as cv
+import numpy as np
 def hytaThreshold(mapped, sdthresh = 0.03, fixedThresh = 0.25):
     '''Takes one-channel decimal image (intensities 0-1) and applies hybrid thresholding.
 
@@ -10,9 +11,10 @@ def hytaThreshold(mapped, sdthresh = 0.03, fixedThresh = 0.25):
 
     mean, stddev = cv.meanStdDev(mapped)
     if stddev < sdthresh:
-        return cv.threshold(mapped, brThresh,255,cv.THRESH_BINARY)
+        return cv.threshold(mapped, brThresh,255,cv.THRESH_BINARY)[1]
     else:
         # normalizes image from 0-1 to 0-255 because otsu threshold doesn't like        decimals for some reason
         mapped = np.array(mapped * 255, dtype='uint8')
-        return cv.threshold(mapped, 0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+        blur = cv.GaussianBlur(mapped,(5,5),0)
+        return cv.threshold(mapped, 0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
         
